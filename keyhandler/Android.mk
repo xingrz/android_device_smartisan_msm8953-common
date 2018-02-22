5c9df1533b7baf88888be539a702b2726f18be1f
Anton Hansson <hansson@google.com>
2018-02-22 17:13:00 +0100

    msm8953: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant package
    Bug: 73535841
    Change-Id: I5196e8c7277ff86280782e7b479a499add2e8f9f

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -23,6 +23,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
