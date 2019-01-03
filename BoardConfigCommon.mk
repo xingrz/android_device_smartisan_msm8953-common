1b0003a2195508ed60858ade3bbfe029cabff46c
XiNGRZ <chenxingyu92@gmail.com>
2019-01-04 02:18:05 +0800

    msm8953: Update mount points to P ways
    Change-Id: Ifdd545e1b9acab206a4de5402cd6ba762c8f61f7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ TARGET_USES_64_BIT_BINDER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 buildvariant=user
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
