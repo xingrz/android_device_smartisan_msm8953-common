b5286b312132fe5be9ceffb1b5d6bbba07393923
XiNGRZ <chenxingyu92@gmail.com>
2019-01-05 00:35:26 +0800

    msm8953: keyhandler: Fix build on P
    * Drop dependency of platform SDK
    * Disable ProGuard
    
    Change-Id: I999cb4b9e749f05c8cfd534b7bc3561e50d23b44

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -27,9 +27,6 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.mokee.platform.internal
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
