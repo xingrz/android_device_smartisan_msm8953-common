a79e7576f569a637e342fae34243baf952ce9d1e
XiNGRZ <chenxingyu92@gmail.com>
2019-05-26 17:13:05 +0800

    msm8953: Add custom bootanimation intro
    Change-Id: I059ee60ec873e73980145e32489a6634ae93099c

--- common.mk
+++ common.mk
@@ -68,6 +68,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Boot animation
+TARGET_BOOTANIMATION_INTRO := $(LOCAL_PATH)/bootanimation
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
