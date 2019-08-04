01fa0fc9a3526c609d718a7a19dc54cc8e82f366
XiNGRZ <chenxingyu92@gmail.com>
2019-08-04 16:33:53 +0800

    msm8953: Drop unused TARGET_USE_SDCLANG
    Change-Id: Ie0da6371ab3e0be6ed282c56fc71ba0fb3e91008

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,9 +23,6 @@
 
 COMMON_PATH := device/smartisan/msm8953-common
 
-# SDClang configuration
-TARGET_USE_SDCLANG := true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8953
 TARGET_NO_BOOTLOADER := true
